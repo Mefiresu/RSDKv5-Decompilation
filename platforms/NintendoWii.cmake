86e8dde0041f1d6e59d28f0710958e36b2b2a430
st×tic <rmgrichdeveloper@gmail.com>
2023-04-14 23:47:43 -0500

    yeah

--- platforms/NintendoWii.cmake
+++ platforms/NintendoWii.cmake
@@ -12,43 +12,47 @@ endif()
 
 set(RETRO_MOD_LOADER OFF CACHE BOOL "Disable the mod loader" FORCE)
 
-pkg_check_modules(OGG ogg)
+# pkg_check_modules(OGG ogg)
+# 
+# if(NOT OGG_FOUND)
+#     set(COMPILE_OGG TRUE)
+#     message(NOTICE "libogg not found, attempting to build from source")
+# else()
+#     message("found libogg")
+#     target_link_libraries(RetroEngine ${OGG_STATIC_LIBRARIES})
+#     target_link_options(RetroEngine PRIVATE ${OGG_STATIC_LDLIBS_OTHER})
+#     target_compile_options(RetroEngine PRIVATE ${OGG_STATIC_CFLAGS})
+# endif()
+# 
+# pkg_check_modules(THEORA theora theoradec)
+# 
+# if(NOT THEORA_FOUND)
+#     message("could not find libtheora, attempting to build manually")
+#     set(COMPILE_THEORA TRUE)
+# else()
+#     message("found libtheora")
+#     target_link_libraries(RetroEngine ${THEORA_STATIC_LIBRARIES})
+#     target_link_options(RetroEngine PRIVATE ${THEORA_STATIC_LDLIBS_OTHER})
+#     target_compile_options(RetroEngine PRIVATE ${THEORA_STATIC_CFLAGS})
+# endif()
 
-if(NOT OGG_FOUND)
-    set(COMPILE_OGG TRUE)
-    message(NOTICE "libogg not found, attempting to build from source")
-else()
-    message("found libogg")
-    target_link_libraries(RetroEngine ${OGG_STATIC_LIBRARIES})
-    target_link_options(RetroEngine PRIVATE ${OGG_STATIC_LDLIBS_OTHER})
-    target_compile_options(RetroEngine PRIVATE ${OGG_STATIC_CFLAGS})
-endif()
-
-pkg_check_modules(THEORA theora theoradec)
-
-if(NOT THEORA_FOUND)
-    message("could not find libtheora, attempting to build manually")
-    set(COMPILE_THEORA TRUE)
-else()
-    message("found libtheora")
-    target_link_libraries(RetroEngine ${THEORA_STATIC_LIBRARIES})
-    target_link_options(RetroEngine PRIVATE ${THEORA_STATIC_LDLIBS_OTHER})
-    target_compile_options(RetroEngine PRIVATE ${THEORA_STATIC_CFLAGS})
-endif()
-
-target_compile_options(RetroEngine PRIVATE 
+set(SHARED_COMPILE
     $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti -fno-threadsafe-statics> 
-    -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables -fmerge-all-constants)
-target_link_options(RetroEngine PRIVATE -Wl,-gc-sections -Wl,--strip-all -Wl,--orphan-handling=discard -Wl,-Map,.map)
-target_compile_definitions(RetroEngine PRIVATE BASE_PATH="/RSDKv5/")
+    -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables -fmerge-all-constants
+)
 
+set (SHARED_LINK
+    -Wl,-gc-sections -Wl,--strip-all -Wl,--orphan-handling=discard -Wl,-Map,.map
+)
+
+target_compile_options(RetroEngine PRIVATE ${SHARED_COMPILE})
+target_compile_definitions(RetroEngine PRIVATE BASE_PATH="/RSDKv5/" RETRO_DISABLE_LOG=1)
+target_link_options(RetroEngine PRIVATE ${SHARED_LINK})
 target_link_libraries(RetroEngine fat aesnd)
 
-target_compile_options(${GAME_NAME} PRIVATE 
-    $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti -fno-threadsafe-statics> 
-    -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables -fmerge-all-constants)
-target_link_options(${GAME_NAME} PRIVATE -Wl,-gc-sections -Wl,--strip-all -Wl,--orphan-handling=discard)
+target_compile_options(${GAME_NAME} PRIVATE ${SHARED_COMPILE})
 target_compile_definitions(${GAME_NAME} PRIVATE RETRO_INCLUDE_EDITOR=0)
+target_link_options(${GAME_NAME} PRIVATE ${SHARED_LINK})
 
 set(PLATFORM Wii)
 
